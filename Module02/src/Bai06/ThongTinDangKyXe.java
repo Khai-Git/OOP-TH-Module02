945704da620bd9dc7c3bcc14e8f08884d77231af
Khai-Git <kelvinnon28092001@gmail.com>
2023-02-28 17:19:39 +0700

    Create Bai06

---
+++ Module02/src/Bai06/ThongTinDangKyXe.java
@@ -0,0 +1,13 @@
+package Bai06;
+
+public class ThongTinDangKyXe {
+	private String chuXe;
+	private String loaiXe;
+	private double triGiaXe;
+	private int dungTichXyLanh;
+	public ThongTinDangKyXe() {
+		this.chuXe = "Chua co";
+		this.loaiXe = "Chua co";
+		
+	}
+}
