abc671365de102b441566ba9310b63ab25e3b834
Khai-Git <kelvinnon28092001@gmail.com>
2023-03-01 11:44:44 +0700

    Update code Bai07: HangThuc Pham: Create Entity: Create Constructor

--- Module02/src/Bai07/HangThucPham.java
+++ Module02/src/Bai07/HangThucPham.java
@@ -1,5 +1,26 @@
 package Bai07;
 
-public class HangThucPham {
+import java.time.LocalDate;
 
+public class HangThucPham {
+	private String maHang;
+	private String tenHang;
+	private double donGia;
+	private LocalDate ngaySanXuat;
+	private LocalDate ngayHetHan;
+	public HangThucPham() {
+		this.maHang = "Trong";
+		this.tenHang = "Trong";
+		this.donGia = 0;
+		
+	}
+	public HangThucPham(String maHang, String tenHang, double donGia, LocalDate ngaySanXuat, LocalDate ngayHetHan) {
+		super();
+		this.maHang = maHang;
+		this.tenHang = tenHang;
+		this.donGia = donGia;
+		this.ngaySanXuat = ngaySanXuat;
+		this.ngayHetHan = ngayHetHan;
+	}
+	
 }
